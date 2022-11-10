a0a928e3dea37aa2ae944401a8ad440b739d4af1
WΔ <w@winterdelta.com>
2022-11-10 14:03:29 +0000

    ishq haveli label

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,23 @@
 import styles from '../styles/page.module.css'
 import Form from '../components/form/form'
 import Posts from '../components/posts'
+import { Noto_Nastaliq_Urdu } from '@next/font/google'
+
+const arabic = Noto_Nastaliq_Urdu({ weight: '400', subsets: ['arabic'] })
 
 export default function Home () {
   return (
     <div>
       <div className={styles.container}>
         <main className={styles.main}>
-          {/* <div className={styles.mantra}>If you know you know</div> */}
+          <div className={styles.mantra}>
+            <div className={styles.mantraOutline}>
+              <div className={styles.mantraUrdu}>
+                <div className={arabic.className}>عشق</div>
+              </div>
+              <div className={styles.mantraEnglish}>HAVELI</div>
+            </div>
+          </div>
           <div className={styles.content}>
             <div className={styles.form}>
               <Form />
